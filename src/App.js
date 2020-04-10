2e028570830cd12b3d803f55c916f212e523a0a1
git-hub <ev15963@hs.ac.kr>
2020-04-11 03:57:06 +0900

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+//import profileImage from './img/telmo-pic.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Myu React App</h1>
+      <h2>Author : ev15963</h2>
+      
+    </div>
+  );
+}
+
+export default App;
