9d3263c05ea7f19d4bb1b5205de525ada78d290a
test <test@naver.com>
2020-05-24 00:14:06 +0900

    두잇!!

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="title">두잇! 리액트 시작하기</h1>
+      <h1 className="title">두잇!! 리액트 시작하기</h1>
     </div>
   );
 }
